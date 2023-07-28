5447aa8ff8ccc7499f8dd4e0ef1a003154677697
Mattias Makra <mattias@makra.org>
2023-07-28 10:35:21 +0200

    Move and refine the args handling.

---
+++ args-functions.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+func printInstructions() {
+	fmt.Println("Usage: dotnet run <source:string> <destination:string> <IncludeDateInFileName:bool (optional)>")
+	fmt.Println("Example: dotnet run --configuration Release https://pe.makra.dev/api/event/getall backup.json")
+}
+
+func formatDestination(destination string) (formattedDestination string) {
+	index := strings.LastIndex(destination, ".")
+	now := time.Now()
+	formattedDestination = fmt.Sprintf("%s_%s%s", destination[:index], now.Format("2006-01-02"), destination[index:])
+	return
+}
+
+func mapArguments(args []string) (couldMap bool, source string, destination string) {
+	if len(os.Args) != 3 {
+		fmt.Println("Incorrect number of arguments passed")
+		printInstructions()
+		couldMap = false
+		return
+	}
+
+	source = os.Args[1]
+	destination = os.Args[2]
+
+	if _, err := url.ParseRequestURI(source); err != nil {
+		fmt.Printf("Incorrectly formatted source Has to be a valid url. Error %s\n", err.Error())
+		printInstructions()
+		couldMap = false
+		return
+	}
+
+	couldMap = true
+	return
+}
